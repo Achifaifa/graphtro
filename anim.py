9039850a2b1642a6534dd3df2761d115f0ae512f
Yuri Numerov <achi@hush.ai>
2016-07-18 22:29:01 +0200

    added rotating cube axis

--- anim.py
+++ anim.py
@@ -7,22 +7,55 @@ import math, random
 grid=[[0 for i in range(50)] for j in range(50)]
 text="""
 Ever seen metaballs done in matplotlib?
-                      Ever seen a python program written in esperanto?
-                      Look at the awesome axis, it's off the charts!!
-                      Greetings go to:   
-    stage7
-    halcy & SVatG
-    Klon, Marcan, Imobilis
-    Euskal Encounter crew & scene people
-                      I'll have a proper demo next time, promise! ;P
+                          Look at the awesome axis, it's off the charts!!
+                          Greetings go to:   
+    stage7 | Yawin | halcy & SVatG | Klon | Marcan | Imobilis | Euskal Encounter crew & scene people
+                          I'll have a proper demo next time, promise! ;P
 """.replace("\n", "")
 
 needed_frames=(len(text)*3)+150
 
+cube=[(-10,10,-10),(10,10,-10),(10,-10,-10),(-10,-10,-10),
+      (-10,10,10), (10,10,10), (10,-10,10), (-10,-10,10) ]
+
+def rotate_3dpoint(p, angle, ax):
+  r = [0, 0, 0]
+  ca = math.cos(angle)
+  sa = math.sin(angle)
+  r[0]+=(ca+(1-ca)*ax[0]*ax[0])*p[0]
+  r[0]+=((1-ca)*ax[0]*ax[1]-ax[2]*sa)*p[1]
+  r[0]+=((1-ca)*ax[0]*ax[2]+ax[1]*sa)*p[2]
+  r[1]+=((1-ca)*ax[0]*ax[1]+ax[2]*sa)*p[0]
+  r[1]+=(ca+(1-ca)*ax[1]*ax[1])*p[1]
+  r[1]+=((1-ca)*ax[1]*ax[2]-ax[0]*sa)*p[2]
+  r[2]+=((1-ca)*ax[0]*ax[2]-ax[1]*sa)*p[0]
+  r[2]+=((1-ca)*ax[1]*ax[2]+ax[0]*sa)*p[1]
+  r[2]+=(ca+(1-ca)*ax[2]*ax[2])*p[2]
+  return r
+
+def rotate_object(obj, angle, axis):
+  """Rotate an object around given axis."""
+  for n,i in enumerate(obj):
+      obj[n] = rotate_3dpoint(i, angle, axis)
+
+def rotcube(step):
+
+  step=int(math.floor(step/5))
+  global cube
+
+  rotate_object(cube, (0.5+0.5*math.sin(step/35))/30  , (0,1,0))
+  rotate_object(cube, (0.75+0.75*math.cos(step/50))/30, (0,0,1))
+  rotate_object(cube, (1+math.sin(step/30))/30        , (1,0,0))
+  grid=[[0 for i in range(50)] for j in range(50)]
+  tempvert=[tuple(j+25 for j in i) for i in cube]
+  for i in tempvert: grid[int(i[0])][int(i[1])]=i[2]
+  return grid
+
 meatt=1.4
 meatgoo=0.95
-def meatballs(grid,step):
+def meatballs(step):
 
+  grid=[[0 for i in range(50)] for j in range(50)]
   step+=1
   meatang=3.14*step/150;
   mbA=[25+math.cos(meatang*5)*20,25+math.sin(meatang*2)*20]
@@ -83,7 +116,8 @@ def init():
 
 def animate(i):
   global grid
-  grid=meatballs(grid, i)
+  # grid=meatballs(i)
+  grid=rotcube(i)
   Z=np.array(grid).astype(np.int)
   c=plt.pcolor(Z, cmap='gbmap')
 
@@ -93,7 +127,7 @@ def animate(i):
   return c
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=2, interval=20, blit=True)
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=60, interval=20, blit=True)
 anim.save('./output/graphtro.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
 #ffmpeg -i input.mp4 -i input.mp3 -c copy -map 0:0 -map 1:0 output.mp4
